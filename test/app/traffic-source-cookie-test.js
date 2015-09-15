ae6bf940f797922fe43b98f7c9d7a432aef0890e
Thiago von Sydow <thiagovs05@gmail.com>
2015-09-15 11:00:40 -0300

    added empty initial test file

---
+++ test/app/traffic-source-cookie-test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+describe("#init", function() {
+
+  beforeEach(function() {
+  });
+
+  describe("when visitor does not have the cookie", function() {
+
+    beforeEach(function() {
+    });
+
+    it("creates cookie", function() {
+    });
+
+  });
+
+  describe("when visitor have the cookie", function() {
+    it("does not create a new cookie", function() {
+    });
+
+  });
+
+});
